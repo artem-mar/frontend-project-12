1b0d956646d022fda123ff9888b5771f7c915d08
Artem M <artemmarin91@mail.ru>
2022-09-20 03:36:03 +0300

    fix NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <Navbar expand="lg" className="shadow-sm bg-white">
     <div className="container">
-      <Navbar.Brand href="/login">Hexlet Chat</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
     </div>
   </Navbar>
 );
