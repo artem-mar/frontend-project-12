b412ecd458c4b62682056cd1c50c0628c02b27f6
Artem M <artemmarin91@mail.ru>
2022-09-28 00:49:35 +0300

    upd NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks/index.js';
 
-const Nav = () => (
-  <Navbar expand="lg" className="shadow-sm bg-white">
-    <div className="container">
-      <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
-    </div>
-  </Navbar>
-);
+const Nav = () => {
+  const { loggedIn, logOut } = useAuth();
+
+  return (
+    <Navbar expand="lg" className="shadow-sm bg-white">
+      <div className="container">
+        <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+        {loggedIn && <Button onClick={logOut}>Log out</Button>}
+      </div>
+    </Navbar>
+  );
+};
 
 export default Nav;
