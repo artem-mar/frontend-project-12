7d012f58ab1d9796c2790b596cce78e3fc3f95a0
Artem M <artemmarin91@mail.ru>
2022-10-05 23:21:41 +0300

    upd Channel.jsx (added handlers)

--- frontend/src/components/Channel.jsx
+++ frontend/src/components/Channel.jsx
@@ -13,12 +13,18 @@ const Channel = ({ channel: { id, name, removable } }) => {
   const activeChannelSwitcher = () => {
     dispatch(actions.setCurrentChannel(id));
   };
+  const deleteChannelHandler = () => {
+    dispatch(actions.openModal({ type: 'removeChannel', id }));
+  };
+  const renameChannelHandler = () => {
+    dispatch(actions.openModal({ type: 'renameChannel', id }));
+  };
 
   return (
     <ButtonGroup className="d-flex">
       <Button
         onClick={activeChannelSwitcher}
-        className="rounded-0 text-start"
+        className="rounded-0 text-start text-truncate"
         type="button"
         variant={variant}
       >
@@ -33,9 +39,9 @@ const Channel = ({ channel: { id, name, removable } }) => {
           variant={variant}
           hidden={!removable}
         >
-          <Dropdown.Item className="py-0" eventKey="1">Удалить</Dropdown.Item>
+          <Dropdown.Item onClick={deleteChannelHandler} className="py-0">Удалить</Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Item className="py-0" eventKey="2">Переименовать</Dropdown.Item>
+          <Dropdown.Item onClick={renameChannelHandler} className="py-0">Переименовать</Dropdown.Item>
         </DropdownButton>
       )}
     </ButtonGroup>
