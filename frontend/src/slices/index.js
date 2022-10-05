1fb76ffceaee4f2f2eed3f736e7e1d653a892124
Artem M <artemmarin91@mail.ru>
2022-10-05 23:27:02 +0300

    reexport slices

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,15 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
-import channelsReducer, { actions as channelsActions } from './channelsSlice.js';
-import messagesReducer, { actions as messagesActions } from './messagesSlice.js';
+import channelsReducer, {
+  actions as channelsActions,
+  fetchData,
+  addChannelThunk,
+  removeChannelThunk,
+  renameChannelThunk,
+} from './channelsSlice.js';
+import messagesReducer, { actions as messagesActions, sendMessageThunk } from './messagesSlice.js';
+import modalReducer, { actions as modalActions } from './modalSlice.js';
 
 export const actions = {
   ...channelsActions,
   ...messagesActions,
+  ...modalActions,
+};
+
+export const thunks = {
+  fetchData,
+  addChannel: addChannelThunk,
+  removeChannel: removeChannelThunk,
+  renameChannel: renameChannelThunk,
+  sendMessage: sendMessageThunk,
 };
 
 export default configureStore({
   reducer: {
     messages: messagesReducer,
     channels: channelsReducer,
+    modal: modalReducer,
   },
 });
