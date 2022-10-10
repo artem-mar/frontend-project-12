a48ac9626b0e756bea97c103a1e3884764308dbd
Artem M <artemmarin91@mail.ru>
2022-10-11 02:08:56 +0300

    fixed unauthorization bug

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
 import Channels from './Channels.jsx';
 import Chat from './Chat.jsx';
 import { thunks } from '../slices/index.js';
+import { useAuth } from '../hooks/index.js';
 import Modal from './Modal.jsx';
 
 const ChatPage = () => {
@@ -12,10 +14,14 @@ const ChatPage = () => {
     dispatch(thunks.fetchData());
   }, [dispatch]);
 
+  const auth = useAuth();
+  const location = useLocation();
+
   return (
     <>
-      <div className="container h-100 my-3 overflow-hidden shadow rounded p-0">
-        <div className="d-flex flex-row h-100">
+      {!auth.loggedIn && <Navigate to="/login" state={{ from: location }} />}
+      <div className="container h-100 my-3 overflow-hidden shadow rounded-3 p-0">
+        <div className="d-flex flex-row h-100 bg-light">
           <div className="col-4 col-md-3 col-lg-2 p-2 pt-5 border-end">
             <Channels />
           </div>
